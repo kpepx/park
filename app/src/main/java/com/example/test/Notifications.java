ba836e79ff28612ce3333c4b11c6d06ad96bf78f
PEPPER <krittapak.jak@gmail.com>
2019-11-16 23:26:43 +0700

    pepper

--- app/src/main/java/com/example/test/Notifications.java
+++ app/src/main/java/com/example/test/Notifications.java
@@ -15,13 +15,37 @@ import java.util.Calendar;
 import android.app.AlarmManager;
 import android.os.SystemClock;
 import android.widget.Toast;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class Notifications extends AppCompatActivity {
-    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
-    private final static String default_notification_channel_id = "default" ;
-    public static String MY_PREFS_NAME= "nameOfSharedPreferences";
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+public class Notifications extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+    private TextView mTextView;
     Button buttonback4; //ปุ่มย้อนไปหน้าsetting
-    Button show;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,61 +57,61 @@ public class Notifications extends AppCompatActivity {
                 startActivity(new Intent(Notifications.this,Setting.class));
             }
         });
-        Button btnCreateNotification = findViewById(R.id. btnCreateNotification ) ;
-        btnCreateNotification.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick (View v) {
-                Intent notificationIntent = new Intent(Notifications.this,
-                        Second.class ) ;
-                notificationIntent.setFlags(Intent. FLAG_ACTIVITY_CLEAR_TOP | Intent. FLAG_ACTIVITY_SINGLE_TOP );
-                PendingIntent resultIntent = PendingIntent. getActivity (Notifications. this, 0 , notificationIntent , 0 );
-                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(Notifications. this, default_notification_channel_id )
-                        .setSmallIcon(R.drawable. ic_launcher_foreground )
-                        .setContentTitle( "Test" )
-                        .setContentIntent(resultIntent)
-                        .setStyle( new NotificationCompat.InboxStyle())
-                        .setContentText( "Hello! This is my first push notification" ) ;
-                NotificationManager mNotificationManager = (NotificationManager)getSystemService(Context. NOTIFICATION_SERVICE );
-                if (android.os.Build.VERSION. SDK_INT >=android.os.Build.VERSION_CODES. O ) {
-                    int importance = NotificationManager. IMPORTANCE_HIGH ;
-                    NotificationChannel notificationChannel = new NotificationChannel( NOTIFICATION_CHANNEL_ID , "NOTIFICATION_CHANNEL_NAME" , importance) ;
-                    mBuilder.setChannelId( NOTIFICATION_CHANNEL_ID ) ;
-                    assert mNotificationManager != null;
-                    mNotificationManager.createNotificationChannel(notificationChannel) ;
-                }
-                assert mNotificationManager != null;
-                mNotificationManager.notify(( int ) System. currentTimeMillis () ,
-                        mBuilder.build()) ;
-            }
-        });
-        show = (Button)findViewById(R.id.btn_show);
-        show.setOnClickListener(new View.OnClickListener() {
+        mTextView = findViewById(R.id.textView);
+
+        Button buttonTimePicker = findViewById(R.id.button_timepicker);
+        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startAlarm(true,true);
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getFragmentManager(), "time picker");
+            }
+        });
+
+        Button buttonCancelAlarm = findViewById(R.id.button_cancel);
+        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
             }
         });
     }
-    private void startAlarm(boolean isNotification, boolean isRepeat) {
-        AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-        Intent myIntent;
-        PendingIntent pendingIntent;
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
 
-        // SET TIME HERE
-        Calendar calendar= Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY,23);
-        calendar.set(Calendar.MINUTE,24);
+        updateTimeText(c);
+        startAlarm(c);
+    }
 
-        Toast.makeText(Notifications.this, "Yes", Toast.LENGTH_SHORT).show();
-        myIntent = new Intent(Notifications.this,AlarmNotificationReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(this,0,myIntent,0);
+    private void updateTimeText(Calendar c) {
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
 
+        mTextView.setText(timeText);
+    }
 
-        if(isRepeat){
-            Toast.makeText(Notifications.this, calendar.getTime().toString(), Toast.LENGTH_SHORT).show();
-            manager.set(AlarmManager.RTC_WAKEUP, SystemClock.elapsedRealtime()+3000,pendingIntent);}
-        else{
-            Toast.makeText(Notifications.this, "Get", Toast.LENGTH_SHORT).show();
-            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY,pendingIntent);}
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+    }
+
+    private void cancelAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        alarmManager.cancel(pendingIntent);
+        mTextView.setText("Alarm canceled");
     }
 }
