abd8a9895a9cdb10e25c9e500da4cd9f5b0015c7
notchap30 <imnotchap30@users.noreply.github.com>
2019-11-17 17:25:27 +0700

    update

--- app/src/main/java/com/example/test/About.java
+++ app/src/main/java/com/example/test/About.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Button;
+import android.widget.Button;
+
 import androidx.annotation.Nullable;
 public class About extends Activity {
     Button buttonback2; //ปุ่มกลับ
