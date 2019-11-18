f6e2c9392d8a8f0a7206bc48693b2b62b44b3428
PEPPER <krittapak.jak@gmail.com>
2019-11-19 01:18:05 +0700

    pepper

--- app/src/main/java/com/example/test/MapExample.java
+++ app/src/main/java/com/example/test/MapExample.java
@@ -1,19 +1,84 @@
 package com.example.test;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
 public class MapExample extends Fragment {
+    public DatabaseReference data;
+    SharedPreferences myPrefs;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.map_example,container,false);
+        View view = inflater.inflate(R.layout.map_example, container, false);
+        myPrefs = this.getActivity().getSharedPreferences("ID", 0);
+        String rfid = myPrefs.getString("rfid","Default");
+        final ImageView image_1 = (ImageView) view.findViewById(R.id.status1);
+        final ImageView image_2 = (ImageView) view.findViewById(R.id.status2);
+        final ImageView image_3 = (ImageView) view.findViewById(R.id.status3);
+        final TextView name = (TextView) view.findViewById(R.id.namePark1);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        data = database.getReference().child("User").child(rfid);
+        data.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String value_place = String.valueOf(map.get("place"));
+                myPrefs.edit().putString("place",value_place).apply();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        String place = myPrefs.getString("place","Default");
+        data = database.getReference().child("Park").child(place);
+        data.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String value_name = String.valueOf(map.get("name"));
+                name.setText(value_name);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        data = database.getReference().child("Park").child(place).child("Carln");
+        data.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String car1 = String.valueOf(map.get("car_1"));
+                String car2 = String.valueOf(map.get("car_2"));
+                String car3 = String.valueOf(map.get("car_3"));
+                if(car1.equals("1")){image_1.setImageResource(R.drawable.red);}
+                else {image_1.setImageResource(R.drawable.green);}
+                if(car2.equals("1")){image_2.setImageResource(R.drawable.red);}
+                else {image_2.setImageResource(R.drawable.green);}
+                if(car3.equals("1")){image_3.setImageResource(R.drawable.red);}
+                else {image_3.setImageResource(R.drawable.green); }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        return view;
     }
 
 }
