b3dbacd9086112a60ce21d691b10cec9b2872d61
PEPPER <krittapak.jak@gmail.com>
2019-12-02 19:11:40 +0700

    pepper

--- app/src/main/java/com/example/test/MapExample.java
+++ app/src/main/java/com/example/test/MapExample.java
@@ -1,5 +1,6 @@
 package com.example.test;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -25,17 +26,22 @@ import java.util.Map;
 public class MapExample extends Fragment {
     public DatabaseReference data;
     SharedPreferences myPrefs;
+    Dialog myDialog;
     Button buttonbackpark;
+    public String value_place;
+    TextView close_txt,slot_txt,available_txt;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_example, container, false);
+        final View view = inflater.inflate(R.layout.map_example, container, false);
+        myDialog = new Dialog(getActivity());
         myPrefs = this.getActivity().getSharedPreferences("ID", 0);
         String rfid = myPrefs.getString("rfid","Default");
         final ImageView image_1 = (ImageView) view.findViewById(R.id.status1);
         final ImageView image_2 = (ImageView) view.findViewById(R.id.status2);
         final ImageView image_3 = (ImageView) view.findViewById(R.id.status3);
         final TextView name = (TextView) view.findViewById(R.id.namePark);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         data = database.getReference().child("User").child(rfid);
         data.addValueEventListener(new ValueEventListener() {
@@ -44,7 +50,7 @@ public class MapExample extends Fragment {
                 Map map = (Map) dataSnapshot.getValue();
                 String value_status = String.valueOf(map.get("status"));
                 if(value_status.equals("in")) {
-                    String value_place = String.valueOf(map.get("place"));
+                    value_place = String.valueOf(map.get("place"));
                     myPrefs.edit().putString("place", value_place).apply();
                     FirebaseDatabase database = FirebaseDatabase.getInstance();
                     data = database.getReference().child("Park").child(value_place);
@@ -133,7 +139,60 @@ public class MapExample extends Fragment {
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
+        Button info = (Button) view.findViewById(R.id.buttoninfo);
+        info.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showpopup(view);
+            }
+        });
         return view;
+    }
+    public void showpopup (View v){
+        TextView txtclose;
+        myDialog.setContentView(R.layout.info);
+        close_txt = (TextView) myDialog.findViewById(R.id.close);
+        slot_txt = (TextView) myDialog.findViewById(R.id.capacity);
+        available_txt = (TextView) myDialog.findViewById(R.id.available);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        data = database.getReference().child("Park").child(value_place);
+        data.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map map = (Map) dataSnapshot.getValue();
+                String value_slot = String.valueOf(map.get("slot"));
+                slot_txt.setText("Capacity: "+value_slot+" cars");
+                final int[] num_available = new int[1];
+                for (int i = 1;i < Integer.parseInt(value_slot); i++) {
+                    final String num_slot = Integer.toString(i);
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    data = database.getReference().child("Park").child(value_place).child("CarIn");
+                    data.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            Map map = (Map) dataSnapshot.getValue();
+                            String value_available = String.valueOf(map.get("car" + num_slot));
+//                            num_available[0] = num_available[0] + Integer.parseInt(value_available);
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+//                available_txt.setText("Available: "+Integer.toString(num_available[0])+" cars");
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+        txtclose = (TextView) myDialog.findViewById(R.id.leave);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
     }
 
 }
